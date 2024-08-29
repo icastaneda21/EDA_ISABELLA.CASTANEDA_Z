f2588c87c22658e91e3c89083ccd249d8fbc5d9e
icastaneda21 <isabella.castaneda.zapata@gmail.com>
2024-08-28 21:07:01 -0500

    Updated

---
+++ arrayhomework.js
@@ -0,0 +1,22 @@
+function esParOImpar(num) {
+    if (num % 2 === 0) {
+      console.log(`${num} es par`);
+    } else {
+      console.log(`${num} es impar`);
+    }
+  }
+  
+  esParOImpar(6); // Salida: 4 es par
+  esParOImpar(9); // Salida: 7 es impar
+
+  const esParOImpar = (num) => {
+    if (num % 2 === 0) {
+      console.log(`${num} es par`);
+    } else {
+      console.log(`${num} es impar`);
+    }
+  }
+  
+  esParOImpar(6); 
+  esParOImpar(9); 
+  
